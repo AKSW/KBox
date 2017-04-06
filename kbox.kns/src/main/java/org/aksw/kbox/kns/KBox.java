49d11477f1bd9253a1d875c8daad118981f9f121
emarx <emarx@live.com>
2017-04-06 14:15:26 +0200

    - moving resolve code from Kibe

--- kbox.kns/src/main/java/org/aksw/kbox/kns/KBox.java
+++ kbox.kns/src/main/java/org/aksw/kbox/kns/KBox.java
@@ -38,7 +38,41 @@ public class KBox extends org.aksw.kbox.KBox {
 	 * @throws Exception if any error occurs during the operation.
 	 */
 	public static URL resolve(KNSServerList knsServerList, URL resourceURL) throws Exception {
-		KNSResolveVisitor resolveVisitor = new KNSResolveVisitor(resourceURL);
+		KNSResolverVisitor resolveVisitor = new KNSResolverVisitor(resourceURL);
+		knsServerList.visit(resolveVisitor);
+		return resolveVisitor.getResolvedURL();
+	}
+	
+	/**
+	 * Resolve the given URL in the available KNS.
+	 * The first KNS to be checked is the default KNS, 
+	 * thereafter the user's KNS.
+	 * 
+	 * @param resourceURL the URL to be resolved by the KNS.
+	 * @param knsServerList list of KNS servers
+	 * 
+	 * @return the resolved URL.
+	 * 
+	 * @throws Exception if any error occurs during the operation.
+	 */
+	public static URL resolve(URL resourceURL, String format, String version) throws Exception {
+		return resolve(new KNSServerList(), resourceURL, format, version);
+	}
+	
+	/**
+	 * Resolve the given URL in the available KNS.
+	 * The first KNS to be checked is the default KNS, 
+	 * thereafter the user's KNS.
+	 * 
+	 * @param resourceURL the URL to be resolved by the KNS.
+	 * @param knsServerList list of KNS servers
+	 * 
+	 * @return the resolved URL.
+	 * 
+	 * @throws Exception if any error occurs during the operation.
+	 */
+	public static URL resolve(KNSServerList knsServerList, URL resourceURL, String format, String version) throws Exception {
+		KNSResolverVisitor resolveVisitor = new KNSResolverVisitor(resourceURL, format, version);
 		knsServerList.visit(resolveVisitor);
 		return resolveVisitor.getResolvedURL();
 	}
@@ -57,5 +91,23 @@ public class KBox extends org.aksw.kbox.KBox {
 	 */
 	public static URL resolve(URL resourceURL, URL knsServerURL) throws Exception {
 		return KNSTable.resolve(resourceURL, knsServerURL);
+	}
+	
+	/**
+	 * Resolve the given URL in the available KNS.
+	 * The first KNS to be checked is the default KNS, 
+	 * thereafter the user's KNS.
+	 * 
+	 * @param resourceURL the URL to be resolved by the KNS.
+	 * @param knsServerURL the URL of the KNS Server.	 
+	 * @param format the KB format. 
+	 * @param version the KB version.
+	 * 
+	 * @return the resolved URL.
+	 * 
+	 * @throws IOException if any error occurs during the operation.
+	 */
+	public static URL resolve(URL resourceURL, URL knsServerURL, String format, String version) throws IOException {
+		return KNSTable.resolve(resourceURL, knsServerURL, format, version);
 	}
 }
