c5b8ff740e130064ebd574311ecf608a15afd017
emarx <emarx@live.com>
2017-04-24 17:57:47 +0200

    - removing unnecessary imports

--- kbox.core/src/main/java/org/aksw/kbox/utils/URLUtils.java
+++ kbox.core/src/main/java/org/aksw/kbox/utils/URLUtils.java
@@ -6,11 +6,10 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.List;
 
 public class URLUtils {
 	public static URL[] stringToURL(String... urls) throws Exception {
-		URL[] urlArray = new URL[urls.length];		
+		URL[] urlArray = new URL[urls.length];
 		for(int i=0; i < urls.length; i++) {
 			urlArray[i]  = new URL(urls[i]);
 		}
