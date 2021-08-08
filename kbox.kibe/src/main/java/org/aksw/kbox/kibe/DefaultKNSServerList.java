539c50a9de2078d26730e222bb1a29342fa1fcc2
sahandilshan <sahandilshansg@gmail.com>
2021-08-08 22:58:22 +0530

    Point default KNS to AKSW/KBox/master/kns/2.0

--- kbox.kibe/src/main/java/org/aksw/kbox/kibe/DefaultKNSServerList.java
+++ kbox.kibe/src/main/java/org/aksw/kbox/kibe/DefaultKNSServerList.java
@@ -9,8 +9,7 @@ import org.aksw.kbox.kns.KNSServerListVisitor;
 public class DefaultKNSServerList extends URLKNSServerList {
 	
 	// Default KNS table URL
-//	private final static String DEFAULT_KNS_TABLE_URL = "https://raw.githubusercontent.com/AKSW/KBox/master/kns/2.0/";
-	private final static String DEFAULT_KNS_TABLE_URL = "https://raw.githubusercontent.com/sahandilshan/KBox/dev/kns/2.0/";
+	private final static String DEFAULT_KNS_TABLE_URL = "https://raw.githubusercontent.com/AKSW/KBox/master/kns/2.0/";
 
 	private CustomKNSServerList customKNSServerList = new CustomKNSServerList();
 	
